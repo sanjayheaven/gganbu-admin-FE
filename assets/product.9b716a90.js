8acc44032668251e1c9aff92c40ade5a819562f1
sanjayheaven <sanjayheaven@163.com>
2022-11-15 17:35:03 +0000

    Deploying to gh-pages from @ sanjayheaven/gganbu-admin-FE@d1ddefae6ff8b45e7b55606e63cea8ff705cb73a 🚀

---
+++ assets/product.9b716a90.js
@@ -0,0 +1,1 @@
+import{W as t}from"./index.d73c0318.js";const d=d=>t.get("/products/getProducts",{params:d}),o=d=>t.post("/products/deleteProduct",{productId:d});export{o as d,d as g};
--- assets/product.cfbaf400.js
+++
@@ -1,1 +0,0 @@
-import{U as t}from"./index.907d85b6.js";const d=d=>t.get("/products/getProducts",{params:d}),o=d=>t.post("/products/deleteProduct",{productId:d});export{o as d,d as g};
