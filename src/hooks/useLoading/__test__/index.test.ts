38dcc8e924066c70a8a7a36ffa0f22e5e9ffccc6
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-12-08 02:25:43 +0800

    test: add useLoading test

---
+++ src/hooks/useLoading/__test__/index.test.ts
@@ -0,0 +1,24 @@
+import { renderHook, act } from "@testing-library/react"
+import useLoading from "../index"
+
+const setUp = (initState?: boolean) => renderHook(() => useLoading(initState))
+
+describe("useLoading", () => {
+  it("test on default value", () => {
+    const { result } = setUp(true)
+    expect(result.current.loading).toBe(true)
+  })
+
+  it("test on methods", async () => {
+    const { result } = setUp()
+    expect(result.current.loading).toBe(false)
+    act(() => {
+      result.current.setLoading(true)
+    })
+    expect(result.current.loading).toBe(true)
+    act(() => {
+      result.current.setLoading(false)
+    })
+    expect(result.current.loading).toBe(false)
+  })
+})
