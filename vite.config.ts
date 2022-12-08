46661aff2e9b79fe57b5a533faaf754e00ecf2d0
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-12-09 00:25:23 +0800

    build: add visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig, type PluginOption } from "vite"
 import react from "@vitejs/plugin-react"
 import viteCompression from "vite-plugin-compression"
+import { visualizer } from "rollup-plugin-visualizer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/", // default '/', need to modify when deploy to GitHub Pages
   server: { port: 9527 },
-  plugins: [react(), viteCompression()],
-
+  plugins: [react(), viteCompression(), visualizer()],
   esbuild: {
     // pure: ["console.log"],
     drop: ["console", "debugger"],
