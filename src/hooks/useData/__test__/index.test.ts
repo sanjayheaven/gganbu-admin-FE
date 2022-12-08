f6ba100cb93bd1a3a145f4bcef5f9e13390bba29
Huang Daoxu <34485325+sanjayheaven@users.noreply.github.com>
2022-12-08 22:48:12 +0800

    test: fix useData

--- src/hooks/useData/__test__/index.test.ts
+++ src/hooks/useData/__test__/index.test.ts
@@ -1,12 +1,12 @@
 import { renderHook, act } from "@testing-library/react"
 import useData from "../index"
 
-const setUp = (initState?: []) => renderHook(() => useData(initState))
+const setUp = (initState?: object[]) => renderHook(() => useData(initState))
 
 describe("useData", () => {
   it("test on methods", async () => {
-    const { result } = setUp()
-    expect(result.current.data).toStrictEqual({ name: "" })
+    const { result } = setUp([{ name: "" }])
+    expect(result.current.data).toStrictEqual([{ name: "" }])
     // act(() => {
     //   result.current.setData({ name: "1" })
     // })
