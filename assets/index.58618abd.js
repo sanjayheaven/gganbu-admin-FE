99c501d006229cca4e7bec60df19fe3d1bb55ed1
sanjayheaven <sanjayheaven@163.com>
2022-12-11 15:50:01 +0000

    Deploying to gh-pages from @ sanjayheaven/gganbu-admin-FE@983b113108846fad39393bb7374dc4bd1848426d 🚀

--- assets/index.58618abd.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index.19492a28.js";function n(s){const[e,t]=o.exports.useState(s);return{selectedItem:e,setSelectedItem:t}}function r(s){const[e,t]=o.exports.useState({onCancel:()=>t({...e,open:!1}),...s});return{modal:e,setModal:t}}export{r as a,n as u};
